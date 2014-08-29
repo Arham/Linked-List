9400c1289792e884bb2212f8a907b777ef9568bf
Arham <amit.pugalia@gmail.com>
2014-08-29 18:41:46 +0530

    utility created

---
+++ src/Node.java
@@ -0,0 +1,7 @@
+/**
+ * Created by apugalia on 8/29/2014.
+ */
+public class Node {
+	int data;
+	Node next;
+}
---
+++ src/Utils.java
@@ -0,0 +1,25 @@
+/**
+ * Created by apugalia on 8/29/2014.
+ */
+public class Utils {
+	public static void displayNodes(Node root) {
+		while (root != null) {
+			System.out.print(root.data + "->");
+			root = root.next;
+		}
+		System.out.print("null");
+	}
+
+	public static Node createNodes(int numOfNodes) {
+		Node root = new Node();
+		root.data = 0;
+		Node current = root;
+		for (int i = 1; i < numOfNodes; i++) {
+			Node temp = new Node();
+			temp.data = i;
+			current.next = temp;
+			current = temp;
+		}
+		return root;
+	}
+}
